41eb252ff6b74d6b632cb2fb556ea1fe29ad5a77
Randiel Melgarejo <randielmelgarejo@gmail.com>
2016-01-23 09:00:41 -0500

    refactorizando el appbundle to application

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-            new Kod\AppBundle\AppBundle(),
+            new Kod\Application\ApplicationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
